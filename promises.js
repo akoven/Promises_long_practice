10fc93e185645955c73efa4d52b4aebe72e64f2b
Attiya <adarensburg@live.com>
2022-03-09 17:06:30 -0600

    phases one through four complete

--- promises.js
+++ promises.js
@@ -1,28 +1,54 @@
 /* ============================== Phase 1 ============================== */
 /* -------------------------- exploring async -------------------------- */
+function num1(){
+    return 1;
+}
 
-// Your code here
+async function num2(){
+    return 2;
+}
+
+console.log('num1', num1());
+console.log('num2', num2());
+
+// num2().then(result => console.log(result));
 
 
 
 /* ============================== Phase 2 ============================== */
 /* -------------------------- exploring await -------------------------- */
 
-// Your code here
+async function waiting(){
+    const value = await num2();
+    console.log('waiting', value);
+}
+waiting();
 
 
 
 /* ============================== Phase 3 ============================== */
 /* --------------------- creating a custom Promise --------------------- */
 
-// Your code here
-
+async function waitForMyPromise(){
+    const promise = new Promise((resolve) => {
+        setTimeout(() => {
+            resolve('done!!!');
+        },1000);
+    });
+    const result = await promise;
+    console.log('my promise is', result);
+}
+waitForMyPromise();
 
 
 /* ============================== Phase 4 ============================== */
 /* -------------------------- exploring then --------------------------- */
+    new Promise((resolve) => {
+        setTimeout(() => {
+            resolve('done!');
+        }, 1500);
+    }).then(response => console.log('then my other promise is',response));
 
-// Your code here
 
 
 
